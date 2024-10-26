9b51512e4e7fae66b70f3443017833e162c303c4
chopchaper42 <vorongri@fel.cvut.cz>
2024-10-26 15:32:11 +0200

    can choose the length of a time period when LED is ON

--- main.s
+++ main.s
@@ -22,51 +22,90 @@
 .set  LONG_PHASE_LENGTH, 0xE1398  @1C2730 @ 0,1 sec * 8 = 0,8 sec
 .set DELAY_LENGTH, 0x8CC3F        @11987E       @ 0,1 sec * 5 = 0,5 sec
 .set PAUSE_LENGTH, 0x384E60       @709CC0       @ 0,1 sec * 20 = 2 sec
+.set DEBOUNCE_TIME, 0x54759
+.set ONE_SECOND_TIME, 0x8CC3F
+.set FOUR_SECOND_TIME, 0x8CC3F @0x2330FC
+.set EIGHT_SECOND_TIME, 0x4661F8
 
 @ R5 - BUTTON_TIMER
 @ R6 - LED_TIMER
+@ R7 - LIGHT_ON flag
+@ R8 - DEBOUNCE_TIMER
 
 _start:
     bl setup_clock
     bl setup_gpio
 
-    mov r5, #0          @ set BUTTON_TIMER to 0
-    mov r6, #0          @ set LED_TIMER to 0
+    mov r5, #0          @ set BUTTON_TIMER to   0
+    mov r6, #0          @ set LED_TIMER to      0
+    mov r7, #0          @ set LIGHT_ON flag to  0
+    mov r8, #0          @ set DEBOUNCE_TIMER to 0
     
 loop:
+    @ test if DEBOUNCE_TIMER is 0
+    teq r8, #0
+
+    @ if not, skip button testing
+    bne btn_pressed
+
+    @ otherwise, test the button
     ldr r0, =GPIOA_IDR  @ Read input of port A
     ldr r1, [r0]        @ load the value stored on address from r0 to r1. r1 = 0x40010808
     tst r1, #1          @ Test if 1st pin is HIGH
-    
-    beq timer           @ if button isnt pressed, skip debouncing
 
+    bne btn_pressed     @ if the button is pressed
+
+    teq r5, #0          @ BUTTON_TIMER == 0?
+
+    bne set_led_timer   @ if BUTTON_TIMER > 0 --> set led timer
+
+                        @ otherwise
+    teq r6, #0          @ is LED_TIMER == 0?
+    ite ne
+    subne r6, #1        @ if not, decrement LED_TIMER
+    bleq green_led_off  @ if yes, turn the LED off
+
+b loop
+
+set_led_timer:
+
+    ldr r2, =ONE_SECOND_TIME
+    cmp r5, r2
+    bgt set_eight_sec
+    ldr r6, =FOUR_SECOND_TIME
+    mov r5, #0          @ clear the BUTTON_TIMER
+
+b loop
+
+set_eight_sec:
+    ldr r6, =EIGHT_SECOND_TIME
+    mov r5, #0          @ clear the BUTTON_TIMER
+
+b loop
+
+btn_pressed:
+    
+    teq r5, #0          @ check if the button was pressed before (BUTTON_TIMER != 0)
+
+    add r5, #1          @ increment BUTTON_TIMER
+
+    bne button_was_pressed_before
+
+    @ init debouncing
+    ldr r8, =DEBOUNCE_TIME
+    
     bl green_led_on     @ if button is pressed, turn the green on
 
+b loop
+
+button_was_pressed_before:
+
+    @ check if need to decrement DEBOUNCER_TIMER
+    teq r8, #0          @ DEBOUNCE_TIMER == 0?
     
-    
-    
+    beq loop            @ if yes, go to loop
 
-    @ debounce
-debounce:
-    ldr r0, =0x8CC3F     @ ~0,5sec
-debounce_internal:
-    subs r0, #1
-    bne debounce_internal
-b timer
-
-
-timer:
-    cmp r5, #0          @ compare TIMER with 0
-    beq on_timer_zero   @ if TIMER == 0, jump to on_timer_zero
-
-    subs r5, #1          @ else subtract 1 from TIMER
-    b loop               @ and jump to loop
-
-
-on_timer_zero:
-
-    
-
+    sub r8, #1          @ otherwise, decrement debounce timer
 b loop
 
 blue_led_on:
